994c94621ba20eb39cb1fb1b6ffe5fbf42dce221
Rebeca Medina <rebeca.medina@ulead.ac.cr>
2020-10-04 21:11:00 -0600

    tareaUno

---
+++ src/variableExamples.java
@@ -0,0 +1,10 @@
+public class variableExamples {
+    public static void main (String[]args){
+
+        short valorMaximo= 32767;
+        long valorEntero= 2233720359L;
+        double valorNoEntero= 1.79769313486232;
+
+        System.out.println (valorNoEntero + valorMaximo % valorEntero);
+    }
+}
